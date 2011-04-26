466a2001f9b74bc85cb4e5316680fad26b90325d
Kerstin Blumenstein <dm101504@fhstp.ac.at>
2011-04-26 14:19:05 +0200

    project added

---
+++ PhoneZxing/Classes/PhoneZxingAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  PhoneZxingAppDelegate.h
+//  PhoneZxing
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright ___ORGANIZATIONNAME___ ___YEAR___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "PhoneGapDelegate.h"
+#import <ZXingWidgetController.h>
+#import <QRCodeReader.h>
+
+@interface PhoneZxingAppDelegate : PhoneGapDelegate {
+}
+
+@end
+
